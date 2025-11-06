887f7b2a93fd885623e42916673917f81f847189
Camilu-png <camila.arancibiaf@sansano.usm.cl>
2025-11-06 12:43:26 -0300

    feat(main): display solution generated using fitness without soft constraints

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src.data_loader import DataLoader
-from src.representation import Solution, TimetableData
+from src.representation import TimetableData
 from src.initial_solution import greedy
-from src.fitness import fitness
+from src.fitness import fitness, fitness_without_soft_contraints
 from src.algorithms.simulated_annealing import simulated_annealing
 from src.baseline import baseline
 import sys
@@ -20,39 +20,58 @@ def main():
     data = TimetableData(**data_dict)
     
     baseline_schedule = baseline(data)
-    bas_fitness, eligible_students = fitness(baseline_schedule, data)
-    print("Baseline Solution Fitness:", bas_fitness)
-    print("Students who can attend the assistantship:", eligible_students)
-    print("Attendance percentage:", round((eligible_students*100)/data.num_students, 2))
+    bas_fitness = fitness(baseline_schedule, data)
+    bas_fitness_count = fitness_without_soft_contraints(baseline_schedule, data)
+    percentage_bas = (bas_fitness_count*100)/data.num_students
+
+    print(f"Baseline Solution Fitness: {bas_fitness}")
+    print(f"Students who can attend the assistantship: {bas_fitness_count}")
+    print(f"Attendance percentage: {round(percentage_bas,2)}%")
     baseline_schedule.view()
     
     # Create initial solution
     initial_solution = greedy(data)
-    val_fitness, eligible_students = fitness(initial_solution, data)
-    print("Initial Solution Fitness:", val_fitness)
-    print("Students who can attend the assistantship:", eligible_students)
-    print("Attendance percentage:", round((eligible_students*100)/data.num_students, 2))
-    initial_solution.view()
 
     print("")
-    # Parameters for Simulated Annealing
+    # # Parameters for Simulated Annealing
+    # initial_temp = 100_000.0
+    # final_temp = 1.0
+    # alpha = 0.98
+    # max_iter = 100_000
+
     initial_temp = 100_000.0
-    final_temp = 1.0
-    alpha = 0.98
+    final_temp = 50.0
+    alpha = 0.95
     max_iter = 100_000
 
-    # Ejecuting Simulated Annealing
-    best_solution = simulated_annealing(
-        initial_solution, initial_temp, final_temp, alpha, max_iter, data
+    # Ejecuting Simulated Annealing whitout soft constraints
+    print("Starting Simulated Annealing without soft constraints...\n")
+    whithout_solution = simulated_annealing(
+        initial_solution, initial_temp, final_temp, alpha, max_iter, data, fitness_without_soft_contraints
     )
-    best_fitness, best_eligible_students = fitness(best_solution, data)
-    print("Initial Solution Fitness:", best_fitness)
-    print("Students who can attend the assistantship:", best_eligible_students)
-    print("Attendance percentage:", round((best_eligible_students*100)/data.num_students, 2))
+    whithout_fitness = fitness(whithout_solution, data)
+    whithout_fitness_count = fitness_without_soft_contraints(whithout_solution, data)
+    percentage_without = (whithout_fitness_count*100)/data.num_students
+
+
+    print(f"Best Solution Fitness without soft constraints: {round(whithout_fitness, 2)}")
+    print(f"Students who can attend the assistantship: {whithout_fitness_count}")
+    print(f"Attendance percentage: {round(percentage_without, 2)}%")
+    whithout_solution.view()
+
+
+    # Ejecuting Simulated Annealing
+    print("\nStarting Simulated Annealing with constraints...\n")
+    best_solution = simulated_annealing(
+        initial_solution, initial_temp, final_temp, alpha, max_iter, data, fitness
+    )
+    best_fitness = fitness(best_solution, data)
+    best_fitness_count = fitness_without_soft_contraints(best_solution, data)
+
+    print(f"Best Solution Fitness: {round(best_fitness, 2)}")
+    print(f"Students who can attend the assistantship: {best_fitness_count}")
+    print(f"Attendance percentage: {round((best_fitness_count*100)/data.num_students, 2)}%")
     best_solution.view()
-
-    data.assistants.view()
-
 
 
 if __name__ == "__main__":
