6e468d488fe0edebbdb0dc59023c400444cb8bdd
Camilu-png <camila.arancibiaf@sansano.usm.cl>
2025-11-06 16:28:37 -0300

    feat(main): add configurations results

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+import sys
+import pathlib
+from datetime import datetime
+from src.baseline import baseline
 from src.data_loader import DataLoader
 from src.representation import TimetableData
 from src.initial_solution import greedy
 from src.fitness import fitness, fitness_without_soft_contraints
 from src.algorithms.simulated_annealing import simulated_annealing
-from src.baseline import baseline
-import sys
 
 
 def main():
@@ -12,27 +14,26 @@ def main():
     print("  Educational Timetabling Solver - v1.0")
     print("==========================================\n")
     path = sys.argv[1]
-    print("Data path:", path)
+    timestamp = datetime.now().strftime("%Y-%m-%d-%H%M%S")
 
     # Data Load
     loader = DataLoader(path)
     data_dict = loader.load_all()
     data = TimetableData(**data_dict)
-    
+
     baseline_schedule = baseline(data)
     bas_fitness = fitness(baseline_schedule, data)
     bas_fitness_count = fitness_without_soft_contraints(baseline_schedule, data)
-    percentage_bas = (bas_fitness_count*100)/data.num_students
+    percentage_bas = (bas_fitness_count * 100) / data.num_students
 
     print(f"Baseline Solution Fitness: {bas_fitness}")
     print(f"Students who can attend the assistantship: {bas_fitness_count}")
-    print(f"Attendance percentage: {round(percentage_bas,2)}%")
+    print(f"Attendance percentage: {round(percentage_bas, 2)}%")
     baseline_schedule.view()
-    
+
     # Create initial solution
     initial_solution = greedy(data)
 
-    print("")
     # # Parameters for Simulated Annealing
     # initial_temp = 100_000.0
     # final_temp = 1.0
@@ -44,21 +45,33 @@ def main():
     alpha = 0.95
     max_iter = 100_000
 
+    path_dic = pathlib.Path("results").joinpath("solutions",path.replace("/", "_"), timestamp)
+    path_dic.mkdir(parents=True, exist_ok=True)
+
+    with path_dic.joinpath("configurations").open("a") as f:
+        f.write(f"Initial temperature: {initial_temp}\nFinal temperature: {final_temp}\nAlpha: {alpha}\nMaximun iterations: {max_iter}\n")
+
     # Ejecuting Simulated Annealing whitout soft constraints
-    print("Starting Simulated Annealing without soft constraints...\n")
+    print("\nStarting Simulated Annealing without soft constraints...\n")
     whithout_solution = simulated_annealing(
-        initial_solution, initial_temp, final_temp, alpha, max_iter, data, fitness_without_soft_contraints
+        initial_solution,
+        initial_temp,
+        final_temp,
+        alpha,
+        max_iter,
+        data,
+        fitness_without_soft_contraints,
     )
     whithout_fitness = fitness(whithout_solution, data)
     whithout_fitness_count = fitness_without_soft_contraints(whithout_solution, data)
-    percentage_without = (whithout_fitness_count*100)/data.num_students
+    percentage_without = (whithout_fitness_count * 100) / data.num_students
 
-
-    print(f"Best Solution Fitness without soft constraints: {round(whithout_fitness, 2)}")
+    print(
+        f"Best Solution Fitness without soft constraints: {round(whithout_fitness, 2)}"
+    )
     print(f"Students who can attend the assistantship: {whithout_fitness_count}")
     print(f"Attendance percentage: {round(percentage_without, 2)}%")
     whithout_solution.view()
-
 
     # Ejecuting Simulated Annealing
     print("\nStarting Simulated Annealing with constraints...\n")
@@ -70,7 +83,9 @@ def main():
 
     print(f"Best Solution Fitness: {round(best_fitness, 2)}")
     print(f"Students who can attend the assistantship: {best_fitness_count}")
-    print(f"Attendance percentage: {round((best_fitness_count*100)/data.num_students, 2)}%")
+    print(
+        f"Attendance percentage: {round((best_fitness_count * 100) / data.num_students, 2)}%"
+    )
     best_solution.view()
 
 
